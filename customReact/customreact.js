0df05f272cba82180e672b1c73f22b39e47f7daf
MunawarAziz <149875349+MunawarAziz@users.noreply.github.com>
2024-08-25 23:23:37 +0500

    new files

---
+++ customReact/customreact.js
@@ -0,0 +1,12 @@
+const reactElement={
+    type: "a",
+    props : {
+        href: "https://accounts.binance.com/",
+        target: "_blank"
+    },
+    children: "click me to vist binance"
+}
+
+
+
+const maincontainer = document.querySelector('#root')
